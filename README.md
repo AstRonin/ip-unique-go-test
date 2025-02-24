a39167d23ed557b8d25c5bceddd87eb6b2b15ae8
AstRonin <astronin@gmail.com>
2025-02-24 17:59:18 +0400

    added test code

---
+++ README.md
@@ -0,0 +1,2 @@
+### Task
+https://github.com/Ecwid/new-job/blob/master/IP-Addr-Counter-GO.md
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <fileName>")
+		return
+	}
+
+	fileName := os.Args[1]
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	countUnique := 0
+	i := 0
+	for scanner.Scan() {
+		leftDiv := i % 1000
+		if leftDiv == 0 {
+			fmt.Println("Left next lines behind: ", i)
+		}
+		ip := scanner.Text()
+
+		file2, err2 := os.Open(fileName)
+		if err2 != nil {
+			fmt.Println("Error opening file 2:", err2)
+			return
+		}
+
+		scanner2 := bufio.NewScanner(file2)
+
+		isFind := false
+		for scanner2.Scan() {
+			ip2 := scanner2.Text()
+			if ip == ip2 {
+				isFind = true
+				break
+			}
+		}
+
+		file2.Close()
+
+		if !isFind {
+			countUnique++
+			fmt.Println("Found uniqueu address: ", countUnique)
+		}
+
+		i++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	fmt.Println("Found uniqueu address: ", countUnique)
+}
